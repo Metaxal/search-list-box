cd1bf38d4a2d5ae700ca8f82091d5886c96a6198
Metaxal <laurent.orseau@gmail.com>
2020-07-25 19:44:36 +0100

    fix deps

--- info.rkt
+++ info.rkt
@@ -1,7 +1,10 @@
 #lang info
 (define collection "search-list-box")
-(define deps '("base"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define deps '("gui-lib"
+               "base"))
+(define build-deps '("gui-doc"
+                     "pict-lib"
+                     "scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/search-list-box.scrbl" ())))
 (define pkg-desc "Description Here")
 (define version "0.0")
